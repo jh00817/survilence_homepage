6a8fef881df0778a27b980e82a6938b3fd0774b5
jh00817 <dreambait011@naver.com>
2023-07-25 00:47:05 +0900

    first commit

---
+++ code/surveillance/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views # views 모듈에서 모든 기능을 import
+
+
+urlpatterns = [
+   path('login/', views.login, name='login'), # url 경로를 설정하는 방식
+
+   path('', views.main, name='main'), 
+
+]
---
+++ code/surveillance/main/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def login(request):
+	template = loader.get_template('login.html') # templates 폴더에 있는 html 파일 정보를 가져온다.
+	return HttpResponse(template.render()) # template을 view에 표현한다.
+
+def main(request):
+	template = loader.get_template('main.html') # templates 폴더에 있는 html 파일 정보를 가져온다.
+	return HttpResponse(template.render()) # template을 view에 표현한다.
