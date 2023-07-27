afd1fe39a90b9363834650e95f839c60f94f6388
jh00817 <dreambait011@naver.com>
2023-07-27 14:38:19 +0900

    finish css

--- code/surveillance/main/urls.py
+++ code/surveillance/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 
    path('', views.main, name='main'), 
 
+   path('dev/', views.dev, name='dev'),
 ]
--- code/surveillance/main/views.py
+++ code/surveillance/main/views.py
@@ -8,3 +8,7 @@ def login(request):
 def main(request):
 	template = loader.get_template('main.html') # templates 폴더에 있는 html 파일 정보를 가져온다.
 	return HttpResponse(template.render()) # template을 view에 표현한다.
+
+def dev(request):
+	template = loader.get_template('dev.html') # templates 폴더에 있는 html 파일 정보를 가져온다.
+	return HttpResponse(template.render()) # template을 view에 표현한다.
